481e5688149844c838b5dc3c54f464c3969be5f6
Umesh Pandhare <umeshpandhare29@gmail.com>
2026-07-15 20:53:32 +0530

    initial commit

--- having_clause.sql
+++ having_clause.sql
@@ -3,8 +3,10 @@
 # WHERE filters rows before grouping.
 # HAVING filters groups after grouping.
 use mydatabase;
+use umesh;
 
-select * from customers;
+select * from employees;
+select * from departments;
 
 # using having clause with group by clause 
 select country ,sum(score) as total_score
@@ -29,4 +31,41 @@ from customers
 where score>0
 group by country 
 having avg(score)>430;
+
+#Q1:- Show departments with more than 2 employees.
+
+select department,count(department)as total_emp
+from employees
+group by department
+having total_emp>2;
+
+#Q2:- Show cities where the average salary is greater than 50,000.
+
+select city,avg(salary)as avg_salary
+from employees
+group by city
+having avg_salary>50000;
+
+#Q3:- Show departments whose total salary exceeds 200,000.
+
+select department,sum(salary) as total_salary
+from employees
+group by department
+having total_salary>200000;
+
+
+#Q4:- Count employees in each city and sort by the highest count.
+
+select city, count(empname)as total_emp
+from employees
+group by city
+order by  total_emp desc;
+
+
+#Q5:- Display department-wise average salary in descending order.
+
+select department, avg(salary) as avg_salary
+from employees
+group by department
+order by  avg_salary desc;
 
